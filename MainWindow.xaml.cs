d6e42d5945d4d2c5355aa854b6b7fe3d1cb40e5b
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2023-10-12 11:43:38 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GA1StephanieLopez
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show("Hello! My Name is Stephanie Lopez and I would like to do something within the tech field after I graduate :D");
+        }
+
+
+    }
+}
